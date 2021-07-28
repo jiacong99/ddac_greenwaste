c60373d3076b3f12f236125497c19855b7d52c06
JiaCong9 <jiacong9@gmail.com>
2021-07-28 14:25:32 +0800

    update : topbar

--- DDAC Assignment/Migrations/DDAC_/DDAC_ContextModelSnapshot.cs
+++ DDAC Assignment/Migrations/DDAC_/DDAC_ContextModelSnapshot.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
 
 namespace DDAC_Assignment.Migrations.DDAC_
 {
